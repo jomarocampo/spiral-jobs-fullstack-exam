fc4ae0566644932b1a04a88b69a739763407ba2e
jutz_jomar <amatz5s0ya@gmail.com>
2019-04-13 12:34:01 +0800

    web, create login, registration and user page / controllers

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './public/controllers/login/login.component';
+import { RegistrationComponent } from './public/controllers/registration/registration.component';
+import { UserComponent } from './admin/controllers/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'admin/user', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './public/controllers/login/login.component';
+import { RegistrationComponent } from './public/controllers/registration/registration.component';
+import { UserComponent } from './admin/controllers/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
